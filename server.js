f782bb1b253afd7f73b06e9655717321bd6a7617
Omkaarr1 <omkargupta702@gmail.com>
2023-10-14 12:13:22 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,166 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+const bodyParser = require('body-parser');
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/Travel', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.once('open', () => {
+  console.log('Connected to MongoDB');
+});
+const Schema = mongoose.Schema;
+
+const travelSchema = new Schema({
+  username: String,
+  fromPlace: String,
+  toPlace: String,
+  dateTime: Date,
+  numOfPeople: Number,
+  modeOfTransport: String,
+  carDriverName: String,
+  carNumber: String,
+  selectedCar: String,
+  numOfSeats: Number,
+  trainNumber: String,
+  trainName: String,
+  busNumber: String,
+  busStandLocation: String,
+  flightNumber: String,
+  flightName: String,
+});
+
+const TravelModel = mongoose.model('Travel', travelSchema);
+
+mongoose.createConnection('mongodb://localhost/Login', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const loginDb = mongoose.connection;
+loginDb.on('error', console.error.bind(console, 'MongoDB Login connection error:'));
+
+loginDb.once('open', () => {
+  console.log('Connected to MongoDB for Login');
+});
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
+});
+
+app.get('/save', (req, res) => {
+  const jsonData = req.query.data;
+  const formData = JSON.parse(jsonData);
+
+  // Create a new document using the Travel model and the form data
+  const travelData = new TravelModel(formData);
+
+  // Save the document to the MongoDB collection using Promises
+  travelData.save()
+    .then(() => {
+      console.log('Form data saved to MongoDB:', travelData);
+      res.send('Form data received and saved to MongoDB');
+    })
+    .catch((err) => {
+      console.error('Error saving data to MongoDB:', err);
+      res.status(500).send('Internal Server Error');
+    });
+});
+
+app.get('/search', (req, res) => {
+  const from = req.query.from;
+  const to = req.query.to;
+  TravelModel.find({ fromPlace: from, toPlace: to })
+    .then((data) => {
+      res.json(data);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({ error: "Internal Server Error" });
+    });
+})
+
+app.get('/searchJSON', (req, res) => {
+  const mode = req.query.mode;
+  const no = req.query.no;
+
+  TravelModel.find({ numOfPeople: parseInt(no), modeOfTransport: mode })
+    .then((data) => {
+      res.json(data);
+    })
+    .catch((err) => {
+      res.status(500).json({ error: "Internal Server Error" });
+    })
+})
+
+app.get('/getHome', (req, res) => {
+  res.sendFile(__dirname + '/public/home.html')
+})
+
+
+const loginSchema = new mongoose.Schema({
+  email: String,
+  username: String,
+  password: String,
+  // Add more fields as needed for your login data
+});
+const LoginModel = loginDb.model('Login', loginSchema);
+
+var currentUser = '';
+
+app.get('/getUsername', (req, res) => {
+  res.send(currentUser);
+})
+
+app.post('/login', async (req, res) => {
+  const { username, password } = req.body;
+
+  try {
+    const user = await LoginModel.findOne({ username, password });
+
+    if (user) {
+      currentUser = username;
+      res.status(200).sendFile(__dirname + "/public/home.html");
+    } else {
+      res.status(401).send('Unauthorized: Invalid username or password');
+    }
+  } catch (err) {
+    console.error('Error while querying the database:', err);
+    res.status(500).send('Internal Server Error');
+  }
+});
+
+
+app.post('/signup', async (req, res) => {
+  const { email, username, password } = req.body;
+
+  try {
+    // Check if the username already exists in the "Login" collection
+    const existingUser = await LoginModel.findOne({ username });
+
+    if (existingUser) {
+      // Username already exists, so respond with a conflict status (409) and a message
+      return res.status(409).send('Conflict: Username already exists');
+    }
+
+    // Username is unique, so create a new document and save it
+    const loginData = new LoginModel({ email, username, password });
+    await loginData.save();
+
+    // Respond with a success status (200) and send the index.html file
+    res.status(200).sendFile(__dirname + '/public/index.html');
+  } catch (err) {
+    console.error('Error while handling signup:', err);
+    res.status(500).send('Internal Server Error');
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
---
+++ test.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// In-memory data store (replace this with a real database in a production environment)
+const users = [
+    { id: 1, username: 'user1' },
+    { id: 2, username: 'user2' },
+];
+
+const friendships = [];
+
+app.get('/', (req, res) => {
+    // Display a list of users and the option to send friend requests
+    res.send(`
+        <h1>Users</h1>
+        <ul>
+            ${users.map(user => `
+                <li>${user.username}
+                    <a href="/send_request/${user.id}">Send Friend Request</a>
+                </li>
+            `).join('')}
+        </ul>
+    `);
+});
+
+app.get('/send_request/:senderId', (req, res) => {
+    const senderId = parseInt(req.params.senderId, 10);
+    const receiverId = 1; // Replace with the actual receiver's ID (e.g., from a form)
+
+    // Create a friend request
+    friendships.push({ senderId, receiverId, status: 'pending' });
+
+    res.send('Friend request sent!');
+});
+
+app.get('/accept_request/:requestId', (req, res) => {
+    const requestId = parseInt(req.params.requestId, 10);
+
+    // Accept a friend request
+    const friendship = friendships.find(f => f.id === requestId);
+    if (friendship) {
+        friendship.status = 'accepted';
+        res.send('Friend request accepted!');
+    } else {
+        res.send('Friend request not found.');
+    }
+});
+
+app.listen(port, () => {
+    console.log(Server is running on http://localhost:${port});
+});
